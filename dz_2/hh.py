a05286d3fba43ee6cbc47ceb95fce2a2cdcea8b6
AlekseyB86 <avbelov86@mail.ru>
2022-01-27 03:10:38 +0300

    les2 -fix

--- dz_2/hh.py
+++ dz_2/hh.py
@@ -12,21 +12,19 @@
 Общий результат можно вывести с помощью dataFrame через pandas. Сохраните в json либо csv.t
 """
 import json
-from pprint import pprint
 
 import requests
 from bs4 import BeautifulSoup as bS
 import pandas
 import re
 
-quest = 'vacancy'
 site = 'hh.ru'
-url = f'https://{site}/search/{quest}/'
+url = f'https://{site}/search/vacancy/'
 
 text = 'python'
 headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
-num_page = 1
+num_page = 0
 params = {
     'items_on_page': '20',
     'text': text,
@@ -35,6 +33,7 @@ params = {
 vacancies = []
 
 while True:
+    params['page'] = num_page
     response = requests.get(url, params=params, headers=headers)
     dom = bS(response.text, 'html.parser')
 
@@ -72,18 +71,22 @@ while True:
                 vacancy_data['compensation_min'] = _compensation[0]
                 vacancy_data['compensation_max'] = _compensation[2]
 
-        vacancy_employer = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-employer'})
+        vacancy_employer = vacancy.find('div', {'class': 'vacancy-serp-item__meta-info-company'})
         vacancy_data['employer'] = vacancy_employer.getText().replace(u'\xa0', u'')
-        vacancy_data['employer_vacancies_list'] = 'https://' + site + vacancy_employer.get('href')
+        # noinspection PyBroadException
+        try:
+            employer_hrf = vacancy_employer.find('a', {'data-qa': 'vacancy-serp__vacancy-employer'}).get('href')
+            vacancy_data['employer_vacancies_list'] = 'https://' + site + employer_hrf
+        except Exception:
+            vacancy_data['employer_vacancies_list'] = None
 
         vacancies.append(vacancy_data)
 
-    print(num_page, len(vacancies))
+    # print(num_page, len(vacancies))
+
     if not dom.find('a', {'data-qa': 'pager-next'}):
         break
     num_page += 1
-
-# pprint(vacancies)
 
 data_frame = pandas.DataFrame(vacancies)
 data_frame.to_csv('vacancies.csv', encoding="utf-8-sig")

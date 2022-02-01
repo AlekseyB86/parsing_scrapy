6b855ba8380495fb16af875966ee04722ddc5d46
AlekseyB86 <avbelov86@mail.ru>
2022-02-01 14:27:42 +0300

    les4 - parser_news.py fix [dz3.task2]

--- dz_3/task2/task2.py
+++ dz_3/task2/task2.py
@@ -1,5 +1,6 @@
 """
-
+Написать функцию, которая производит поиск и выводит на экран вакансии с заработной платой больше введённой суммы
+(необходимо анализировать оба поля зарплаты). То есть цифра вводится одна, а запрос проверяет оба поля
 """
 
 from pprint import pprint
@@ -9,8 +10,9 @@ client = MongoClient('127.0.0.1', 27017)
 db = client['vacancies']
 
 num = 500000
+currency = 'руб'
 
-for item in db.vacancy.find({'compensation.currency': 'руб',
+for item in db.vacancy.find({'compensation.currency': currency,
                              '$or': [{'compensation.min': {'$gt': num}},
                                      {'compensation.max': {'$gt': num}}]}):
     pprint(item)
---
+++ dz_4/parser_news.py
@@ -0,0 +1,47 @@
+from pprint import pprint
+from lxml import html
+import requests
+from pymongo import MongoClient
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
+
+client = MongoClient('127.0.0.1', 27017)
+db = client['news_db']
+news = db.news
+news.drop()
+
+
+def get_lenta_news():
+    url = 'https://lenta.ru/'
+    response = requests.get(url, headers=headers)
+
+    dom = html.fromstring(response.text)
+    items = dom.xpath('//div/a[contains(@class,"card-") and contains(@class,"_topnews")]')
+
+    news_list = []
+    for item in items:
+        title = item.xpath('.//*[contains(@class,"_title")]/text()')[0]
+        link = url + item.get('href')
+        date = '.'.join([s for s in item.get('href').split('/') if s.isdigit()])
+        source = 'lenta.ru'
+        news_data = {
+            'title': title,
+            'link': link,
+            'date': date,
+            'source': source
+        }
+        news_list.append(news_data)
+
+        # noinspection PyBroadException
+        try:
+            news.insert_one(news_data)
+        except Exception:
+            pass
+
+    return news_list
+
+
+if __name__ == '__main__':
+    get_lenta_news()
+    for doc in news.find({}):
+        pprint(doc)
